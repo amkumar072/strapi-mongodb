543012165fa80804ccf23809a33b5490ec5cb3bc
sgeek <sgeek017@gmail.com>
2021-01-19 03:27:47 +0000

    mongodb uri updated

--- config/database.js
+++ config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
       settings: {
         // host: env('DATABASE_HOST', '127.0.0.1'),
         srv: env.bool('DATABASE_SRV', true),
-        uri: env('DATABASE_URI', 'mongodb://localhost:27017/strapi-test-mongodb'),
-        // uri: env('DATABASE_URI', 'mongodb+srv://admin:admin@cluster0.t2xcx.mongodb.net/strapi-test-mongodb?retryWrites=true&w=majority'),
+        // uri: env('DATABASE_URI', 'mongodb://localhost:27017/strapi-test-mongodb'),
+        uri: env('DATABASE_URI', 'mongodb+srv://admin:admin@cluster0.t2xcx.mongodb.net/strapi-test-mongodb?retryWrites=true&w=majority'),
         // port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'strapi-test-mongodb'),
         // username: env('DATABASE_USERNAME', null),
@@ -15,8 +15,8 @@ module.exports = ({ env }) => ({
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),  // uncomment  for local
-        // ssl: env.bool('DATABASE_SSL', true), // uncomment  for cloud
+        // ssl: env.bool('DATABASE_SSL', false),  // uncomment  for local
+        ssl: env.bool('DATABASE_SSL', true), // uncomment  for cloud
 
       },
     },
